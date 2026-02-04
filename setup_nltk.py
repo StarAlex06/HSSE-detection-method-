67c8b9bc539b77be792de9086a0a8f65de88e483
Староверов Алексей <alexeystaroverov1@gmail.com>
2026-02-04 20:03:47 +0300

    Changed datasets and deleted some unneccesary files

--- setup_nltk.py
+++
@@ -1,9 +0,0 @@
-import nltk
-
-# Скачиваем необходимые ресурсы NLTK
-nltk.download('punkt')
-nltk.download('averaged_perceptron_tagger')
-nltk.download('wordnet')
-nltk.download('omw-eng')
-
-print("NLTK resources downloaded successfully!")
